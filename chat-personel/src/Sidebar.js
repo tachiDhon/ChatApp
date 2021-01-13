e5e5daa050cfb785039fd01edb6724f59c67a25e
TashiDHUNDUP <tashi.dhundup@epitech.eu>
2021-01-13 17:51:54 +0100

    adding project chat-personel written in react.js

---
+++ chat-personel/src/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Sidebar.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+
+
+function Sidebar() {
+   return (
+      <div className="sidebar ">
+         <div className="sidebar_header">
+            <Avatar />
+            <div className="sidebar_headerRight">
+               <IconButton>
+                  <DonutLargeIcon />
+               </IconButton>
+               <IconButton>
+                  <ChatIcon />
+               </IconButton>
+               <IconButton>
+                  <MoreVertIcon />
+               </IconButton>
+            </div>
+         </div>
+
+         <div className="sidebar_search">
+            <SearchOutlined />
+            <input placeholder="Search.." type="text" />
+         </div>
+
+         <div className="sidebar_chats">
+
+         </div>
+      </div>
+   )
+}
+
+export default Sidebar
---
+++ chat-personel/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyBKGuFZzLqoAmV7KDzFomO-aoyyOcl7izQ",
+   authDomain: "whatsappclone-e550f.firebaseapp.com",
+   databaseURL: "https://whatsappclone-e550f.firebaseio.com",
+   projectId: "whatsappclone-e550f",
+   storageBucket: "whatsappclone-e550f.appspot.com",
+   messagingSenderId: "249734952538",
+   appId: "1:249734952538:web:4cc2d7d4ab43003c7e0a63",
+   measurementId: "G-N8MJCR6TRT"
+};
