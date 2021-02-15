fd5a7591d10107185490e6573f9f47d452b2cb2b
TashiDHUNDUP <tashi.dhundup@epitech.eu>
2021-02-15 18:17:04 +0100

    continuation sur projet perso

--- chat-personel/src/Chat.js
+++ chat-personel/src/Chat.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 import './Chat.css';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 
 function Chat() {
-   const [seed, setSeed] = useState("");
+   const [seed, setSeed] = useState(""); 
 
    useEffect(() => {
       setSeed(Math.floor(Math.random() * 5000));
@@ -35,11 +37,22 @@ function Chat() {
          </div>
 
          <div className="chat_body">
+            <p className={`chat_message ${true && 'chat_receiver'}`}>
+               <span className="chat_name">Tashi Dhundup</span>
+               Hey guys
 
+               <span className="chat_timestamp">3:52pm</span>
+            </p>
+            
          </div>
 
          <div className="chat_footer">
-
+            <InsertEmoticonIcon />
+            <form>
+               <input type="text" />
+               <button>Send</button>
+            </form>
+            <MicIcon />
          </div>
       </div>
    )
